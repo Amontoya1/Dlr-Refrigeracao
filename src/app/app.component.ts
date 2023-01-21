3eea1e7506b0d9d42977002b459fc2dc0b3586ed
andrea.montoya@ed.company <terceiro-aruiz@icatuseguros.com.br>
2023-01-21 19:42:15 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dlr-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Dlr';
+}
